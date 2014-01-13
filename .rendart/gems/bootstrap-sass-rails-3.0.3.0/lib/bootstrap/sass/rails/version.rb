30d7a147040dbb70996c1bb388a847857bb189c5
Luke Parham <lappp9@gmail.com>
2014-01-12 20:37:08 -0600

    trying to deploy

--- .rendart/gems/bootstrap-sass-rails-3.0.3.0/lib/bootstrap/sass/rails/version.rb
+++
@@ -1,7 +0,0 @@
-module Bootstrap
-  module Sass
-    module Rails
-      VERSION = '3.0.3.0'
-    end
-  end
-end
--- .rendart/gems/nokogiri-1.6.1/ports/x86_64-apple-darwin13.0.2/libxslt/1.1.26/lib/xsltConf.sh
+++
@@ -1,7 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/Users/lukeparham/Projects/rendArt/.rendart/gems/nokogiri-1.6.1/ports/x86_64-apple-darwin13.0.2/libxslt/1.1.26/lib"
-XSLT_LIBS="-lxslt  -L/Users/lukeparham/Projects/rendArt/.rendart/gems/nokogiri-1.6.1/ports/x86_64-apple-darwin13.0.2/libxml2/2.8.0/lib -lxml2 -lz -lpthread -liconv -lm "
-XSLT_INCLUDEDIR="-I/Users/lukeparham/Projects/rendArt/.rendart/gems/nokogiri-1.6.1/ports/x86_64-apple-darwin13.0.2/libxslt/1.1.26/include"
-MODULE_VERSION="xslt-1.1.26"
