691af3512805ec1beb1ea9053e2de3305937879c
Luke Parham <lappp9@gmail.com>
2014-01-19 16:54:48 -0600

    starting mailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,8 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+
+  def projects
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ RendArt::Application.routes.draw do
   root "static_pages#home"
 
   match "/contact", to: "static_pages#contact", via: "get"
+  match "/projects", to: "static_pages#projects", via: "get"
   match "/emails", to: "emails#new", via: "post"
 
 
