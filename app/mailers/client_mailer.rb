72da7ab9985827aa09db9cc02e8a58e047a43f6d
Luke Parham <lappp9@gmail.com>
2014-01-26 20:49:02 -0600

    not sure this is a good idea

--- app/mailers/client_mailer.rb
+++ app/mailers/client_mailer.rb
@@ -1,5 +1,6 @@
 class ClientMailer < ActionMailer::Base
-  default to: "bmartin@rendartstl.com"
+  #default to: "bmartin@rendartstl.com"
+  default to: "lappp9@gmail.com"
 
   def client_email(client_email, message)
     @client = client_email
